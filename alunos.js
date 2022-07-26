7fd14ab35e955edb9c88c0317c3be5e8e68c9f5b
tthtails <thaslesouza@gmail.com>
2022-07-26 09:14:18 -0300

    Java script do trabalho

---
+++ alunos.js
@@ -0,0 +1,121 @@
+function carregarAlunos() {
+    var corpoTabela = document.querySelector(".corpoTabela");
+
+    for (var i = 0; i < vetAlunos.length; i++) {
+        var mediaAluno = 0;
+        var percentualFaltas = 0;
+
+        var tr = document.createElement("tr")
+        var tdNomeAluno = document.createElement("td")
+        var tdNomeCurso = document.createElement("td")
+        var tdNota1 = document.createElement("td")
+        var tdNota2 = document.createElement("td")
+        var tdParticipacao = document.createElement("td")
+        var tdFalta = document.createElement("td")
+        var tdMedia = document.createElement("td")
+        var tdPercentualFaltas = document.createElement("td")
+        var tdAprovacao = document.createElement("td")
+
+        tdNomeAluno.textContent = vetAlunos[i];
+        tdNomeCurso.textContent = vetCursos[i];
+        tdNota1.textContent = vetProva1[i];
+        tdNota2.textContent = vetProva2[i];
+        tdParticipacao.textContent = vetParticipacao[i];
+        tdFalta.textContent = vetFalta[i] + "h";
+
+        mediaAluno = (vetProva1[i] * 20 + vetProva2[i] * 40 + vetParticipacao[i] * 40) / 100;
+        tdMedia.textContent = mediaAluno;
+
+        percentualFaltas = (vetFalta[i] / 50) * 100
+        tdPercentualFaltas.textContent = percentualFaltas.toFixed(1) + "%"
+
+        tdAprovacao.textContent = "Aprovado!";
+
+        if (mediaAluno < 60) {
+            tdAprovacao.textContent = "Reprovado por nota!";
+        }
+        else {
+            if (percentualFaltas > 25) {
+                tdAprovacao.textContent = "Reprovado por falta!"
+            }
+        }
+
+        tr.appendChild(tdNomeAluno)
+        tr.appendChild(tdNomeCurso)
+        tr.appendChild(tdNota1)
+        tr.appendChild(tdNota2)
+        tr.appendChild(tdParticipacao)
+        tr.appendChild(tdFalta)
+        tr.appendChild(tdMedia)
+        tr.appendChild(tdPercentualFaltas)
+        tr.appendChild(tdAprovacao)
+
+        corpoTabela.appendChild(tr)
+    }
+}
+
+var inNome = document.getElementById("inNome");
+
+function filtrarNome() {
+
+    var nomeAluno = inNome.value;
+
+
+    var corpoTabela = document.querySelector(".corpoTabela");
+
+    corpoTabela.innerText = "";
+
+    for (var i = 0; i < vetAlunos.length; i++) {
+        if (vetAlunos[i].toLocaleLowerCase().indexOf(nomeAluno.toLocaleLowerCase()) != -1) {
+            var mediaAluno = 0;
+            var percentualFaltas = 0;
+
+            var tr = document.createElement("tr")
+            var tdNomeAluno = document.createElement("td")
+            var tdNomeCurso = document.createElement("td")
+            var tdNota1 = document.createElement("td")
+            var tdNota2 = document.createElement("td")
+            var tdParticipacao = document.createElement("td")
+            var tdFalta = document.createElement("td")
+            var tdMedia = document.createElement("td")
+            var tdPercentualFaltas = document.createElement("td")
+            var tdAprovacao = document.createElement("td")
+
+            tdNomeAluno.textContent = vetAlunos[i];
+            tdNomeCurso.textContent = vetCursos[i];
+            tdNota1.textContent = vetProva1[i];
+            tdNota2.textContent = vetProva2[i];
+            tdParticipacao.textContent = vetParticipacao[i];
+            tdFalta.textContent = vetFalta[i] + "h";
+
+            mediaAluno = (vetProva1[i] * 20 + vetProva2[i] * 40 + vetParticipacao[i] * 40) / 100;
+            tdMedia.textContent = mediaAluno;
+
+            percentualFaltas = (vetFalta[i] / 50) * 100
+            tdPercentualFaltas.textContent = percentualFaltas.toFixed(1) + "%"
+
+            tdAprovacao.textContent = "Aprovado!";
+
+            if (mediaAluno < 60) {
+                tdAprovacao.textContent = "Reprovado por nota!";
+            }
+            else {
+                if (percentualFaltas > 25) {
+                    tdAprovacao.textContent = "Reprovado por falta!"
+                }
+            }
+
+            tr.appendChild(tdNomeAluno)
+            tr.appendChild(tdNomeCurso)
+            tr.appendChild(tdNota1)
+            tr.appendChild(tdNota2)
+            tr.appendChild(tdParticipacao)
+            tr.appendChild(tdFalta)
+            tr.appendChild(tdMedia)
+            tr.appendChild(tdPercentualFaltas)
+            tr.appendChild(tdAprovacao)
+
+            corpoTabela.appendChild(tr)
+        }
+    }
+}
---
+++ cursos.js
@@ -0,0 +1,24 @@
+var slCursos = document.getElementById("slCursos");
+var btConfirmar = document.getElementById("btConfirmar");
+
+function carregarCursos() {
+    var cursos = slCursos.value;
+    var corpoTabela = document.querySelector(".corpoTabela");
+    corpoTabela.textContent = "";
+    for (var i = 0; i < vetCursos.length; i++) {
+        if (vetCursos[i].indexOf(cursos) != -1) {
+            var tr = document.createElement("tr");
+            var tdNomeCurso = document.createElement("td");
+            var tdNomeAluno = document.createElement("td");
+
+            tdNomeCurso.textContent = vetCursos[i];
+            tdNomeAluno.textContent = vetAlunos[i];
+
+            tr.appendChild(tdNomeCurso)
+            tr.appendChild(tdNomeAluno)
+
+            corpoTabela.appendChild(tr)
+        }
+    }
+}
+btConfirmar.addEventListener("click", carregarCursos);
