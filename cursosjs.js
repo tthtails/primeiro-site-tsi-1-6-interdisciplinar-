da41556845a112942f22d00160826a552c2be2d0
tthtails <thaslesouza@gmail.com>
2022-07-27 08:28:18 -0300

    atualização do javascript

--- cursosjs.js
+++ cursosjs.js
@@ -1,10 +1,17 @@
+
 var slCursos = document.getElementById("slCursos");
 
 function carregarCursos() {
     var cursos = slCursos.value;
     var corpoTabela = document.querySelector(".corpoTabela");
     corpoTabela.textContent = "";
-    for (var i = 0; i < vetCursos.length; i++) {
+
+    for (var i = 0; i < vetAlunos.length; i++) {
+
+        var mediaAluno = 0;
+        var percentualFaltas = 0;
+
+        
         if (vetCursos[i].indexOf(cursos) != -1) {
             var tr = document.createElement("tr");
             var tdNomeCurso = document.createElement("td");
@@ -13,8 +20,14 @@ function carregarCursos() {
             tdNomeCurso.textContent = vetCursos[i];
             tdNomeAluno.textContent = vetAlunos[i];
 
-            tr.appendChild(tdNomeCurso)
-            tr.appendChild(tdNomeAluno)
+            mediaAluno = (vetProva1[i] * 20 + vetProva2[i] * 40 + vetParticipacao[i] * 40) / 100;
+
+            percentualFaltas = (vetFalta[i] / 50) * 100;
+
+            if (mediaAluno > 60 && percentualFaltas < 25) {
+                tr.appendChild(tdNomeCurso)
+                tr.appendChild(tdNomeAluno)
+            }
 
             corpoTabela.appendChild(tr)
         }
